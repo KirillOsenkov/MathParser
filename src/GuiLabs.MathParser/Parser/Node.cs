43b0df2bc5adb3a51cc9f02bf80857a2cdb1e432
Kirill Osenkov <github@osenkov.com>
2017-09-02 21:31:22 -0700

    Add a .ToString() on Node.

--- src/GuiLabs.MathParser/Parser/Node.cs
+++ src/GuiLabs.MathParser/Parser/Node.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace GuiLabs.MathParser
 {
@@ -19,6 +20,40 @@ namespace GuiLabs.MathParser
             this.Kind = nodeType;
             this.Token = token;
             this.Children = new List<Node>();
+        }
+
+        private string ToString(int childIndex)
+        {
+            return "(" + Children[childIndex].ToString() + ")";
+        }
+
+        public override string ToString()
+        {
+            switch (Kind)
+            {
+                case NodeType.Negation:
+                    return $"-{ToString(0)}";
+                case NodeType.Addition:
+                    return $"{ToString(0)} + {ToString(1)}";
+                case NodeType.Subtraction:
+                    return $"{ToString(0)} - {ToString(1)}";
+                case NodeType.Multiplication:
+                    return $"{ToString(0)} * {ToString(1)}";
+                case NodeType.Division:
+                    return $"{ToString(0)} / {ToString(1)}";
+                case NodeType.Power:
+                    return $"{ToString(0)} ^ {ToString(1)}";
+                case NodeType.PropertyAccess:
+                    return $"{ToString(0)}.{Token.Text}";
+                case NodeType.FunctionCall:
+                    return Token.Text + "(" + string.Join(", ", Children.Select(c => c.ToString())) + ")";
+                case NodeType.Constant:
+                    return Token.Text;
+                case NodeType.Variable:
+                    return Token.Text;
+                default:
+                    return string.Join(" ", Children.Select(c => c.ToString()));
+            }
         }
     }
 }
